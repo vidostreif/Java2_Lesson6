4d62e02b1d81d9a3fe24c4a71e51ebab721fb2e1
vido <vido1@yandex.ru>
2018-07-19 14:57:33 +0400

    периименование класса

--- src/vido/geekbrains/Server.java
+++ src/vido/geekbrains/Server.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Main {
+public class Server {
     static ServerSocket serv = null;
     static Socket sock = null;
     static PrintWriter pw = null;
